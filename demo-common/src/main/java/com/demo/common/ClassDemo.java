0f9037c78f05c804c52c558ccda07f0f31ae7eb7
liuli17 <liuli17@staff.weibo.com>
2020-08-10 19:36:03 +0800

    init

---
+++ demo-common/src/main/java/com/demo/common/ClassDemo.java
@@ -0,0 +1,14 @@
+package com.demo.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ClassDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ClassDemo.class);
+
+    public static void main(String[] args) {
+        log.info("hello");
+    }
+
+}
